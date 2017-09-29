39afbd1d4615b5379b30d717f3d34d118cb79943
eric1221bday <efang@andrew.cmu.edu>
2017-09-29 15:20:27 +0800

    add template package

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# mavros_excercise
-For writing new ROS and MAVROS nodes
+# ROS and MAVROS Template
+This is a template ROS package containing a single ROS node with the appropriate package dependencies for writing a basic MAVROS control node.
---
+++ src/waypoint_tracker.cpp
@@ -0,0 +1,18 @@
+//
+// Created by Eric Fang on 7/17/17.
+//
+
+#include <ros/ros.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <mavros_msgs/CommandBool.h>
+#include <mavros_msgs/SetMode.h>
+#include <mavros_msgs/State.h>
+#include <iostream>
+#include <fstream>
+#include <boost/algorithm/string.hpp>
+
+int main(int argc, char **argv)
+{
+
+    return 0;
+}
